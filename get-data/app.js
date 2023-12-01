d4217ee63bd128489da33a62bb51b6ca78a87bca
kemalege <cirpanliege@gmail.com>
2023-12-01 23:23:55 +0300

    Add comment lines for get-data

--- get-data/app.js
+++ get-data/app.js
@@ -1,5 +1,6 @@
 import getData from "./getData.js";
 
+// This code block fetches user data using the getData function and logs it to the console.
 (async () => {
   try {
     const userData = await getData(1);
--- get-data/getData.js
+++ get-data/getData.js
@@ -1,17 +1,24 @@
 import axios from "axios";
 
+// Function to fetch user data and posts data based on userId
 async function getData(userId) {
   try {
+    // Fetch user data and posts data concurrently using Promise.all
     const [userResponse, postsResponse] = await Promise.all([
       axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`),
       axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`),
     ]);
+
+    // Combine user data and posts data into a single object
     const combinedData = {
       ...userResponse.data,
       posts: postsResponse.data,
     };
+
+    // Return the combined data
     return combinedData;
   } catch (err) {
+    // Log and reject any errors that occur during the data fetching process
     console.error(err);
     return Promise.reject(err);
   }
